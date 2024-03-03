2d60e7f1df339cdb9a5eb4a1a4041dd8818c5cdd
mihdifynn <mihdi.fynn@gmail.comm>
2024-03-03 10:26:05 +0700

    link tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import styles from './styles';
+
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
 import './App.css';
 
-function App() {
+
+
+
+
+
+export default function App(): React.ReactElement {
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#651fff'
+      }
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={styles.container}>
+        <Box sx={styles.parent}>
+          <Box sx={styles.header}>
+            <Avatar
+              src="/faktaalam.png"
+              alt="fakta alam"
+              sx={styles.avatar}
+            />
+
+            <Box typography="subtitle1">
+              <b>FAKTA ALAM</b>
+            </Box>
+          </Box>
+
+          <Button
+            variant="contained"
+            sx={styles.button}
+            href="https://youtube.com/@FAKTAALAMSHORTS?sub_confirmation=1"
+            fullWidth
+          >
+            Youtube
+          </Button>
+
+          <Button
+            variant="contained"
+            sx={styles.button}
+            href="https://tiktok.com/@faktaalamtiktok"
+            fullWidth
+          >
+            Tiktok
+          </Button>
+
+          <Button
+            variant="contained"
+            sx={styles.button}
+            href="https://instagram.com/faktaalamreels"
+            fullWidth
+          >
+            Instagram
+          </Button>
+
+          <Button
+            variant="contained"
+            sx={styles.button}
+            startIcon={<FavoriteBorderIcon />}
+            href="https://www.buymeacoffee.com/xlance.official"
+            fullWidth
+          >
+            Support Us
+          </Button>
+
+          <Box sx={styles.footer}>
+            Owned by&nbsp;
+            
+            <Link
+              underline="hover"
+              href="/"
+            >
+              Xlance
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/styles.tsx
@@ -0,0 +1,40 @@
+const borderRadius = 4;
+
+const styles = {
+    header: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: 2,
+        p: 2
+    },
+    avatar: {
+        width: 80,
+        height: 80
+    },
+    parent: {
+        bgcolor: 'divider',
+        borderRadius,
+        p: 2,
+        maxWidth: 400
+    },
+    container: {
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        bgcolor: 'background.paper'
+    },
+    button: {
+        borderRadius,
+        mt: 2,
+        p: 2,
+    },
+    footer: {
+        typography: 'caption',
+        textAlign: 'center',
+        mt: 2
+    }
+}
+
+export default styles;
